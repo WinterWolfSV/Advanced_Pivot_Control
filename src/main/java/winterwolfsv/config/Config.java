99b59b5e4575a9243982c9882d2971d125e13d57
WinterWolfSV <what112222@gmail.com>
2023-03-01 22:14:54 +0100

    Added pitch and yaw commands

---
+++ src/main/java/winterwolfsv/config/Config.java
@@ -0,0 +1,13 @@
+//package winterwolfsv.config;
+//
+//import me.shedaniel.clothconfig2.api.ConfigBuilder;
+//import net.minecraft.client.gui.screen.Screen;
+//import net.minecraft.text.Text;
+//import net.minecraft.text.TranslatableTextContent;
+//
+//public class Config {
+//    private Screen parent;
+//    ConfigBuilder builder = ConfigBuilder.create()
+//            .setParentScreen(parent)
+//            .setTitle(new Text("config.winterwolfsv.title"));
+//}
