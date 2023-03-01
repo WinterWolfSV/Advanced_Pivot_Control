162f1f660cbe159499e9a5b77e8d79728bd27449
WinterWolfSV <what112222@gmail.com>
2023-03-01 06:56:19 +0100

    Initial commit

---
+++ src/main/java/winterwolfsv/pitch_and_yaw_control/Pitch_And_Yaw_Control.java
@@ -0,0 +1,10 @@
+package winterwolfsv.pitch_and_yaw_control;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pitch_And_Yaw_Control implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/winterwolfsv/pitch_and_yaw_control/client/Pitch_And_Yaw_ControlClient.java
@@ -0,0 +1,13 @@
+package winterwolfsv.pitch_and_yaw_control.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class Pitch_And_Yaw_ControlClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
