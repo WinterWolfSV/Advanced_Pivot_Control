0e1ab96ae3dbd526b4aea50a01ad516a40e377b0
WinterWolfSV <what112222@gmail.com>
2023-03-08 18:51:18 +0100

    Bug fixes

--- src/main/java/winterwolfsv/advancedpivotcontrol/AdvancedPivotControl.java
+++ src/main/java/winterwolfsv/advancedpivotcontrol/AdvancedPivotControl.java
@@ -3,6 +3,7 @@ package winterwolfsv.advancedpivotcontrol;
 import net.fabricmc.api.ModInitializer;
 import winterwolfsv.advancedpivotcontrol.client.Commands;
 
+
 public class AdvancedPivotControl implements ModInitializer {
     @Override
     public void onInitialize() {
